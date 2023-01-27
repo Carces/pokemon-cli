127fc34e757288b48e78b5c796471348b2073e28
Carces <theo_johnson@ntlworld.com>
2023-01-27 11:34:44 +0000

    added doBattle game loop to battle.js, tested working

--- battle.js
+++ battle.js
@@ -10,17 +10,70 @@ class Battle {
         this.otherTrainer = trainerTwo;
         this.turnNumber = 1;
         this.battleOver = false;
+        this.victor = null;
+        this.loser = null;
     }
+
+    doBattle() {
+        while (!this.battleOver) {
+            this.inBetweenTurns();
+            this.fight(
+                this.currentTrainer.currentPokeball.storage, 
+                this.otherTrainer.currentPokeball.storage, 
+            );
+            this.changeCurrentTrainer();
+            if (this.currentTrainer.currentPokeball.storage.health) {
+                this.fight(
+                    this.currentTrainer.currentPokeball.storage, 
+                    this.otherTrainer.currentPokeball.storage, 
+                );
+            }
+            this.checkIfFightOver(this.currentTrainer, this.otherTrainer);
+            this.checkIfFightOver(this.otherTrainer, this.currentTrainer);
+            this.turnNumber++
+        }
+        this.doEndOfBattle()
+    }
+
+    doEndOfBattle() {
+        console.log(`${this.victor.name} defeated ${this.loser.name}!`);
+    }
+
+    checkIfFightOver(trainerA, trainerB) {
+        if (!trainerA.currentPokeball.storage.health) {
+            const trainerBelt = trainerA.belt
+
+            for (let i = 0; i < trainerBelt.length; i++) {
+                const pokeball = trainerBelt[i]
+                if (pokeball.storage && pokeball.storage.health) {
+                    trainerA.currentPokeball = 
+                    trainerBelt[i]
+                    trainerA.currentPokeball.throw()
+                    return;
+                }
+            }
+
+            this.victor = trainerB;
+            this.loser = trainerA;
+            this.battleOver = true;
+            return;
+        }
+    }
+
+    inBetweenTurns() {
+        console.log('-------')
+        console.log(`Turn ${this.turnNumber}`)
+        console.log('-------')
+    }
+
+
 
     fight(attackingPokemon, defendingPokemon) {
         const attacker = attackingPokemon.name;
         const defender = defendingPokemon.name;
-        
-        // Display turn number
-        console.log('-------')
-        console.log(`Turn ${this.turnNumber}`)
-        console.log('-------')
-        // with separators
+
+        let isTarget = defendingPokemon.health > 0
+
 
         // Calculate attacking pokemon's damage,
         const baseDamage = attackingPokemon.useMove();
@@ -29,89 +82,57 @@ class Battle {
         0.75 * baseDamage : baseDamage;
         // accounting for type weaknesses
 
+        if (isTarget){
+            // Deal damage to defending pokemon
+            defendingPokemon.takeDamage(damage);
+            const defendingHealthRatio  = defendingPokemon.health / defendingPokemon.hitPoints;
+            // and calculate how badly damaged it is now
 
-        // Deal damage to defending pokemon
-        defendingPokemon.takeDamage(damage);
-        const defendingHealthRatio  = defendingPokemon.health / defendingPokemon.hitPoints;
-        // and calculate how badly damaged it is now
+            if (damage > baseDamage) console.log ("It's super effective!");
+            if (damage < baseDamage) console.log ("It's not very effective...");
+            
+            // Separator for clearer output
+            console.log('...')
+            //////////////////////////////
 
-        if (damage > baseDamage) console.log ("It's super effective!");
-        if (damage < baseDamage) console.log ("It's not very effective...");
-        
-        // Separator for clearer output
-        console.log('...')
-        //////////////////////////////
+            console.log(`${this.currentTrainer.name}'s pokemon ${attacker} dealt ${damage} damage`)
+            console.log(`${this.currentTrainer.name}'s pokemon ${defender} has ${defendingPokemon.health} hit points remaining`)
 
-        console.log(`${attacker} dealt ${damage} damage`)
-        console.log(`${defender} has ${defendingPokemon.health} hit points remaining`)
+            console.log( 
+                !defendingPokemon.health ? `${defender} fainted!`
+                : defendingHealthRatio > 0.8 ? `${defender} doesn't have a scratch...`
+                : defendingHealthRatio > 0.5 ? `${defender} is slightly damaged!`
+                : defendingHealthRatio > 0.25 ? `${defender} is heavily damaged!`
+                : `${defender} is barely standing!`);
+        }   
 
-        console.log( 
-            !defendingPokemon.health ? `${defender} fainted!`
-            : defendingHealthRatio > 0.8 ? `${defender} doesn't have a scratch...`
-            : defendingHealthRatio > 0.5 ? `${defender} is slightly damaged!`
-            : defendingHealthRatio > 0.25 ? `${defender} is heavily damaged!`
-            : `${defender} is barely standing!`);
-        
-
-            // If the attack causes the defending pokemon to faint...
-        if (!defendingPokemon.health) {
-            let flag = 0;
-            const defenderBelt = this.otherTrainer.belt
-
-            for (let i = 0; i < defenderBelt.length; i++) {
-                const pokeball = defenderBelt[i]
-                if (pokeball.storage && pokeball.storage.health) {
-                    flag = 1;
-                    this.otherTrainer.currentPokeball = 
-                    defenderBelt[i]
-                    this.otherTrainer.currentPokeball.throw()
-                }
-            }
-            // ...check defending trainer's belt for a pokeball containing 
-            // a pokemon that isn't at 0hp, then throw the pokeball and set 
-            // trainerXPokemon property to the pokemon returned by throw()
-
-
-            // If no pokeballs in defender's belt contain a conscious pokemon...
-            if (!flag) {
-                this.battleOver = true;
-                console.log(`${this.currentTrainer.name} defeated ${this.otherTrainer.name}!`);
-            }
-            // end the battle and log a trainer defeated message
+        else {
+            console.log(`${attacker}'s ${attackingPokemon.move} missed because there was nothing to hit!`);
         }
-
-        // Swap currenTrainer and otherTrainer and increment turnNumber...
-        [this.currentTrainer,this.otherTrainer]=[this.otherTrainer,this.currentTrainer]
-        this.turnNumber++
-        // ... ready for next turn!
-
     }
 
-    goToNextTurn() {
-        this.fight(
-            this.currentTrainer.currentPokeball.storage, 
-            this.otherTrainer.currentPokeball.storage, 
-            );
+    changeCurrentTrainer() {
+        [this.currentTrainer,this.otherTrainer]=[this.otherTrainer,this.currentTrainer]
     }
 }
 
 
 
 
-// const jeb = new Trainer('Jebediah')
-// const butch = new Trainer('Butch')
+const jeb = new Trainer('Jebediah')
+const butch = new Trainer('Butch')
 
 
-// const phil = new Charmander('Phil', 10, 2)
-// const paula = new Charmander('Paula', 2, 2)
-// const gerty = new Squirtle('Gerty', 10, 3)
+const phil = new Charmander('Phil', 10, 2)
+const paula = new Charmander('Paula', 2, 2)  
+const gerty = new Squirtle('Gerty', 10, 3)
 
-// jeb.catch(gerty)
-// butch.catch(phil)
-// butch.catch(paula)
+jeb.catch(gerty)
+butch.catch(phil)
+butch.catch(paula)
 
-// const testBattle = new Battle(jeb, butch)
+const testBattle = new Battle(jeb, butch)
 
-// testBattle.goToNextTurn()
+testBattle.doBattle()
 
 
