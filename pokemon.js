f1b84ece8544cb62a599a5f2a75820cfde111341
Carces <theo_johnson@ntlworld.com>
2023-02-06 23:39:15 +0000

    changes to pokemon.js, adding more properties and modifying useMove to use new damage formula

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,14 @@
 class Pokemon {
-    constructor(name, hitPoints, attackDamage, type = 'normal', move = 'tackle') {
+    constructor(name, level = 1, type = 'normal', move = 'tackle', catchDifficulty = 5, hitPoints = level*7.5, attack = level*5, defence = level*5 ) {
         this.name = name;
-        this.hitPoints = hitPoints;
-        this.health = hitPoints;
-        this.attackDamage = attackDamage;
+        this.level = level
         this.type = type;
         this.move = move;
+        this.catchDifficulty = catchDifficulty;
+        this.hitPoints = hitPoints;
+        this.health = hitPoints;
+        this.attack = attack;
+        this.defence = defence;
     }
 
     isEffectiveAgainst(otherPokemon) {
@@ -33,9 +36,11 @@ class Pokemon {
         this.health = Math.max(0, this.health - damage)
     }
 
-    useMove() {
+    useMove(target) {
+        const random = (Math.random() * -0.3) + 1.5
+        const damage = (random * this.attack) - target.defence
         console.log(`${this.name} used ${this.move}!`)
-        return this.attackDamage
+        return damage.toFixed(2)
     }
 
     hasFainted() {
