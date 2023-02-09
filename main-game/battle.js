7dd34c1530ad55920d475b61f36821f4d62e7167
Carces <theo_johnson@ntlworld.com>
2023-02-09 01:17:20 +0000

    lots of refactoring and restructuring to lay the groundwork for adding more systems. most changes are to the format of pokemon properties and can be seen in pokemon.js. some battle testing done, further needed but seems stable

--- main-game/battle.js
+++ main-game/battle.js
@@ -6,40 +6,55 @@ const { Charmander } = require('../pokemon/species/charmander.js');
 const { Squirtle } = require('../pokemon/species/squirtle.js');
 const { Bulbasaur } = require('../pokemon/species/bulbasaur.js');
 const { Rattata } = require('../pokemon/species/rattata.js')
+const { movesData } = require('../pokemon/moves-data.js')
 
 class Battle {
-    constructor(trainerOne, trainerTwo) {
-        this.trainerOne = trainerOne;
-        this.trainerTwo = trainerTwo;
-        this.currentTrainer = trainerOne;
-        this.otherTrainer = trainerTwo;
+    constructor(player, opponent) {
+        this.player = player;
+        this.opponent = opponent;
+        this.playerPokemon = this.player.currentPokeball.storage
+        this.opponentPokemon = this.opponent.currentPokeball.storage
+        // this.currentTrainer = player;
+        // this.currentTrainerPokemon = this.currentTrainer.currentPokeball.storage;
+        // this.otherTrainer = opponent;
+        // this.otherTrainerPokemon = this.otherTrainer.currentPokeball.storage;
         this.turnNumber = 1;
         this.battleOver = false;
-        this.victor = null;
+        this.winner = null;
         this.loser = null;
     }
 
     startBattle() {
         console.log('\n');
-        console.log(`${this.trainerTwo.name} wants to fight!`);
+        console.log(`${this.opponent.name} wants to fight!`);
         console.log('\n');
         this.setCurrentPokeballs()
         this.doBattle()
     }
     resolveTurn() {
-        this.changeCurrentTrainer();
-        if (this.currentTrainer.currentPokeball.storage.health) {
-            this.fight(
-                this.currentTrainer.currentPokeball.storage, 
-                this.otherTrainer.currentPokeball.storage, 
-            );
-        }
-        this.checkIfBattleOver(this.currentTrainer, this.otherTrainer).then(() => {
-            this.checkIfBattleOver(this.otherTrainer, this.currentTrainer).then(() => {
-                this.turnNumber++
-                this.changeCurrentTrainer();
-                if (!this.battleOver) this.doBattle()
-                else this.doEndOfBattle()
+        this.checkIfBattleOver(this.player).then(() => {
+            this.checkIfBattleOver(this.opponent).then(() => {
+                // this.changeCurrentTrainer();
+
+                if (this.opponentPokemon.hitPoints.current) {
+            // TO ADD: check if hp ratio is above x% and use healing item if they have one and trainer is not wildPokemon, try and run in other situation etc.
+                    const moves = this.opponentPokemon.moves
+                    const randomIndex = Math.floor(Math.random()*moves.length)
+                    const randomMove = movesData[moves[randomIndex]]
+                    this.fight(
+                        randomMove,
+                        this.opponentPokemon, 
+                        this.playerPokemon, 
+                    );
+                }
+                this.checkIfBattleOver(this.player).then(() => {
+                    this.checkIfBattleOver(this.opponent).then(() => {
+                        this.turnNumber++
+                        // this.changeCurrentTrainer();
+                        if (!this.battleOver) this.doBattle()
+                        else this.doEndOfBattle()
+                    })
+                })
             })
         })
     }
@@ -52,12 +67,12 @@ class Battle {
         else {
             const pokemonListWithHP = trainer.pokemonList.map(pokeName => {
                 const pokemon = trainer.getPokemon(pokeName).pokemonObj
-                return `${pokeName}: ${pokemon.health}/${pokemon.hitPoints}`
+                return `${pokeName}: ${pokemon.hitPoints.current}/${pokemon.hitPoints.max}`
             })
             
             const pokemonChoices = pokemonListWithHP.map(pokemonWithHp => {
                 const pokemonObj = trainer.getPokemon(pokemonWithHp.split(':')[0]).pokemonObj
-                return !pokemonObj.health ? 
+                return !pokemonObj.hitPoints.current ? 
                     { name: pokemonWithHp, disabled: 'unconscious'}
                 : pokemonObj.name === trainer.currentPokeball.storage.name ?
                     { name: pokemonWithHp, disabled: 'already out'}
@@ -74,6 +89,7 @@ class Battle {
                 const selectedPokemon = answers.pokemonChoice.split(':')[0]
                 const selectedPokemonIndex = trainer.getPokemon(selectedPokemon).index
                 trainer.currentPokeball = trainer.belt[selectedPokemonIndex]
+                this.playerPokemon = this.player.currentPokeball.storage
                 trainer.currentPokeball.throw()
             })
         }
@@ -85,14 +101,21 @@ class Battle {
             name: 'action',
             message: `Choose your action...`,
             choices: ['Fight', 'Pokemon', 'Item', 'Run'],
-        }])
+        },
+        {
+            type: 'list',
+            name: 'move',
+            message: `Choose your move...`,
+            choices: this.playerPokemon.moves,
+            when: (answers) => answers.action === 'Fight'
+          }])
         .then((answers) => {
             if (answers.action === 'Fight') {
-                this.fight(this.currentTrainer.currentPokeball.storage, this.otherTrainer.currentPokeball.storage);
+                this.fight(movesData[answers.move], this.playerPokemon, this.opponentPokemon);
                 this.resolveTurn()
             }
             else if (answers.action === 'Pokemon') {
-                this.choosePokemon(this.currentTrainer).then(() => {
+                this.choosePokemon(this.player).then(() => {
                     this.resolveTurn()
                 })
             }
@@ -102,22 +125,24 @@ class Battle {
     setNextPokemon(trainer) {
       for (let i = 0; i < trainer.belt.length; i++) {
           const pokeball = trainer.belt[i]
-          if (pokeball.storage && pokeball.storage.health) {
+          if (pokeball.storage && pokeball.storage.hitPoints.current) {
               trainer.currentPokeball = trainer.belt[i];
-              return true // tells setCurrentPokeballs that this trainer hasValidPokemon (i.e. with > 0 health)
+              this.opponentPokemon = this.opponent.currentPokeball.storage
+              this.playerPokemon = this.player.currentPokeball.storage
+              return true // tells setCurrentPokeballs that this trainer hasValidPokemon (i.e. with > 0 hitPoints)
           }
       }
       return false // otherwise tells setCurrentPokeballs false
     }
     setCurrentPokeballs(trainer) {
 
-      if (!trainer) { // when called with no arguments in startBattle, throw out first pokemon with health for each trainer
-          this.setNextPokemon(this.currentTrainer)
-          this.setNextPokemon(this.otherTrainer)
-          this.currentTrainer.currentPokeball.throw();
-          this.otherTrainer.currentPokeball.throw();
+      if (!trainer) { // when called with no arguments in startBattle, throw out first pokemon with hitPoints for each trainer
+          this.setNextPokemon(this.player)
+          this.setNextPokemon(this.opponent)
+          this.player.currentPokeball.throw();
+          this.opponent.currentPokeball.throw();
       } 
-      else {  // when called with one specific trainer in checkIfBattleOver, check if that trainer has any pokemon with health
+      else {  // when called with one specific trainer in checkIfBattleOver, check if that trainer has any pokemon with hp
           const currentPokeballIndex = trainer.belt.indexOf(trainer.currentPokeball)
           const hasRemainingPokemon = this.setNextPokemon(trainer)
           trainer.currentPokeball = trainer.belt[currentPokeballIndex] // Quick and dirty solution, maybe a better way to refactor this later. This was causing problems with choosing pokemon because setNextPokemon changes currentPokeball - first available pokemon is set as currentPokeball before player chooses a pokemon to send out, so one of their choices is incorrectly shown as 'already out'
@@ -135,22 +160,22 @@ class Battle {
     }
 
     doEndOfBattle() {
-        console.log(`${this.victor.name} defeated ${this.loser.name}!`);
+        console.log(`${this.winner.name} defeated ${this.loser.name}!`);
     }
 
-    checkIfBattleOver(trainerA, trainerB) {
-        //trainerA.currentPokeball.storage prevents errors if checkIfBattleOver is called and setCurrentPokeballs has not correctly initialized, only possible in dev / testing environment eg. if setCurrentPokeballs is invoked only after a trainer's pokemon are all fainted and there is no pokemon in the default current pokeball
-        if (trainerA.currentPokeball.storage && !trainerA.currentPokeball.storage.health) { 
+    checkIfBattleOver(trainer) {
+        //trainer.currentPokeball.storage prevents errors if checkIfBattleOver is called and setCurrentPokeballs has not correctly initialized, only possible in dev / testing environment eg. if setCurrentPokeballs is invoked only after a trainer's pokemon are all fainted and there is no pokemon in the default current pokeball
+        if (trainer.currentPokeball.storage && !trainer.currentPokeball.storage.hitPoints.current) { 
             
-            return this.setCurrentPokeballs(trainerA).then(() => {
-                if(!trainerA.currentPokeball.storage.health){
-                    this.victor = trainerB;
-                    this.loser = trainerA;
+            return this.setCurrentPokeballs(trainer).then(() => {
+                if(!trainer.currentPokeball.storage.hitPoints.current){
+                    this.winner = trainer === this.opponent ? this.player : this.opponent
+                    this.loser = trainer === this.opponent ? this.opponent : this.player
                     this.battleOver = true;
                 }
             })
         }
-        else { // If pokemon still has health, return an empty promise so that the function that called checkIfBattleOver can use .then on whatever checkIfBattleOver returns without errors
+        else { // If pokemon still has hp, return an empty promise so that the function that called checkIfBattleOver can use .then on whatever checkIfBattleOver returns without errors
             return Promise.resolve()
         }
     }
@@ -166,58 +191,64 @@ class Battle {
     }
 
 
-    fight(attackingPokemon, defendingPokemon) {
+    fight(move, attackingPokemon, defendingPokemon) {
         const attacker = attackingPokemon.name;
         const defender = defendingPokemon.name;
-
-        let hasValidTarget = defendingPokemon.health > 0
+        let hasValidTarget = defendingPokemon.hitPoints.current > 0
 
         // Calculate attacking pokemon's damage,
-        const baseDamage = attackingPokemon.useMove(defendingPokemon);
-        let damage = attackingPokemon.isEffectiveAgainst(defendingPokemon) ?
-        1.25 * baseDamage : attackingPokemon.isWeakTo(defendingPokemon) ? 
+        const baseDamage = attackingPokemon.useMove(move, defendingPokemon);
+        let damage = defendingPokemon.isResistantTo(move) ?
+        1.25 * baseDamage : defendingPokemon.isWeakTo(move) ? 
         0.75 * baseDamage : baseDamage;
         // accounting for type weaknesses
 
         const isCriticalHit = this.getCriticalHit();
         damage += (isCriticalHit * 2);
-
-        const finalDamage = damage.toFixed(2)
+        const finalDamage = +damage.toFixed(2)
 
         if (hasValidTarget){
             // Deal damage to defending pokemon
             defendingPokemon.takeDamage(finalDamage);
-            const defendingHealthRatio  = defendingPokemon.health / defendingPokemon.hitPoints;
+            const defendingHitPointRatio  = defendingPokemon.hitPoints.current / defendingPokemon.hitPoints.max;
             // and calculate how badly damaged it is now
 
-            if (attackingPokemon.isEffectiveAgainst(defendingPokemon)) console.log ("It's super effective!");
-            if (attackingPokemon.isWeakTo(defendingPokemon)) console.log ("It's not very effective...");
+            if (defendingPokemon.isResistantTo(move)) console.log ("It's super effective!");
+            if (defendingPokemon.isWeakTo(move)) console.log ("It's not very effective...");
             if (isCriticalHit) console.log("It's a critical hit!")
             
             // Separator for clearer output
             console.log('\n')
             //////////////////////////////
 
-            console.log(`${this.currentTrainer.name}'s pokemon ${attacker} dealt ${finalDamage} damage`)
-            console.log(`${this.otherTrainer.name}'s pokemon ${defender} has ${defendingPokemon.health} hit points remaining`)
+            console.log(`${attacker} dealt ${finalDamage} damage`)
+            console.log(`${defender} has ${defendingPokemon.hitPoints.current} hit points remaining`)
 
             console.log('\n')
 
             console.log( 
-                !defendingPokemon.health ? `${defender} fainted!`
-                : defendingHealthRatio > 0.8 ? `${defender} barely has a scratch...`
-                : defendingHealthRatio > 0.5 ? `${defender} is slightly damaged!`
-                : defendingHealthRatio > 0.25 ? `${defender} is heavily damaged!`
+                !defendingPokemon.hitPoints.current ? `${defender} fainted!`
+                : defendingHitPointRatio > 0.8 ? `${defender} barely has a scratch...`
+                : defendingHitPointRatio > 0.5 ? `${defender} is slightly damaged!`
+                : defendingHitPointRatio > 0.25 ? `${defender} is heavily damaged!`
                 : `${defender} is barely standing!`);
         }   
 
         else {
-            console.log(`${attacker}'s ${attackingPokemon.move} missed because there was nothing to hit!`);
+            console.log(`${attacker}'s ${move} missed because there was nothing to hit!`);
         }
     }
 
     changeCurrentTrainer() {
-        [this.currentTrainer,this.otherTrainer]=[this.otherTrainer,this.currentTrainer]
+        [this.currentTrainer,
+        this.otherTrainer] =
+            [this.otherTrainer,
+            this.currentTrainer]
+
+        [this.currentTrainerPokemon,
+        this.otherTrainerPokemon] =
+            [this.otherTrainerPokemon,
+            this.currentTrainerPokemon]
     }
 }
 
@@ -232,20 +263,26 @@ const butch = new Trainer('Butch')
 const phil = new Charmander('Phil', 1)
 const paula = new Charmander('Paula', 1) 
 
-gerty.takeDamage(7)
-maude.takeDamage(7)
-phil.takeDamage(7)
-paula.takeDamage(7)
 // console.log(gerty)
+
+gerty.takeDamage(11)
+maude.takeDamage(11)
+phil.takeDamage(11)
+paula.takeDamage(11)
+
+console.log(gerty)
+
 jeb.catch(gerty)
 jeb.catch(maude)
 butch.catch(phil)
 butch.catch(paula)
 
-gerty.health = gerty.hitPoints
-maude.health = maude.hitPoints
-phil.health = phil.hitPoints
-paula.health = paula.hitPoints
+// console.log(gerty)
+
+gerty.hitPoints.current = gerty.hitPoints.max
+maude.hitPoints.current = maude.hitPoints.max
+phil.hitPoints.current = phil.hitPoints.max
+paula.hitPoints.current = paula.hitPoints.max
 
 // console.log(jeb.pokemonList)
 // console.log(jeb.belt)
